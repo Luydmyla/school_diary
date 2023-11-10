277dd37620e6c6b8084a08d07b926c79a5fe9215
Liudmyla <87442018+Luydmyla@users.noreply.github.com>
2023-11-10 02:22:56 +0200

    addedsome styles

--- src/components/CustomInput/CustomInput.jsx
+++ src/components/CustomInput/CustomInput.jsx
@@ -2,7 +2,7 @@ import css from "./CustomInput.module.css";
 
 export const CustomInput = ({ value }) => {
   return (
-    <input type="submit" value="вибрати" className={css.input}>
+    <input type="submit" value="Вибрати" className={css.input}>
       {value}
     </input>
   );
--- src/components/Lessons/LessonsCard.jsx
+++ src/components/Lessons/LessonsCard.jsx
@@ -5,18 +5,18 @@ import css from "./Lessons.module.css";
 
 export const LessonsCard = ({ title, lessons }) => (
   <table className={css.table}>
-    <tbody>
-      <tr>
-        <td>
-          <h2 className={css.Lessons_title}> {title}</h2>
-        </td>
+    <tbody className={css.table_body}>
+      <tr className={css.table_tr}>
+        {/* <td> */}
+        <h2 className={css.Lessons_title}> {title}</h2>
+        {/* </td> */}
       </tr>
       {lessons.map((lesson, index) => (
-        <tr key={index}>
-          <td>
+        <tr key={index} className={css.table_tr}>
+          <td className={css.table_td_name}>
             <span className={css.info_table_name}> {lesson.name}</span>
           </td>
-          <td>
+          <td className={css.table_td_task}>
             <span className={css.info_table_task}>{lesson.task}</span>
           </td>
         </tr>
